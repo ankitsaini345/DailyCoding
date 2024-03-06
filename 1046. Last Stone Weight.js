3e7fd4a659b06501c78d810a20c529d306749df5
Ankit Saini <ankitsaini345@gmail.com>
2024-03-06 18:49:30 +0530

    maxHeap

---
+++ 1046. Last Stone Weight.js
@@ -0,0 +1,75 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function (stones) {
+  const maxHeap = [];
+  for (let i = 0; i < stones.length; i++) {
+    const element = stones[i];
+    maxHeap.push(element);
+    bubbleUp(maxHeap);
+  }
+
+  while (maxHeap.length) {
+    const first = remove(maxHeap);
+    if (!maxHeap.length) return first;
+    const second = remove(maxHeap);
+    if (first == second){
+        if(!maxHeap.length) return 0;
+        continue;
+    }
+    else insert(maxHeap, Math.abs(first - second));
+  }
+};
+
+function insert(maxHeap, val) {
+  maxHeap.push(val);
+  bubbleUp(maxHeap);
+}
+
+function remove(maxHeap) {
+  const item = maxHeap[0];
+  swap(maxHeap, 0, maxHeap.length - 1);
+  maxHeap.pop();
+  bubbleDown(maxHeap);
+  return item;
+}
+
+function bubbleUp(maxHeap, i = maxHeap.length - 1) {
+  const parent = i === 0 ? -1 : Math.trunc((i - 1) / 2);
+  if (parent < 0) return;
+
+  if (maxHeap[parent] < maxHeap[i]) {
+    swap(maxHeap, parent, i);
+    bubbleUp(maxHeap, parent);
+  }
+}
+
+function bubbleDown(maxHeap, parent = 0) {
+  const left = 2 * parent + 1;
+  const right = 2 * parent + 2;
+
+  if (right < maxHeap.length) {
+    if (maxHeap[right] >= maxHeap[left] && maxHeap[right] > maxHeap[parent]) {
+      swap(maxHeap, right, parent);
+      bubbleDown(maxHeap, right);
+    } else if (
+      maxHeap[left] >= maxHeap[right] &&
+      maxHeap[left] > maxHeap[parent]
+    ) {
+      swap(maxHeap, left, parent);
+      bubbleDown(maxHeap, left);
+    }
+  } else if (left < maxHeap.length && maxHeap[left] > maxHeap[parent]) {
+    swap(maxHeap, left, parent);
+    bubbleDown(maxHeap, left);
+  }
+}
+
+function swap(array, i, j) {
+  const temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+console.log(lastStoneWeight([2,2]));
